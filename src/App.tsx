02574dfa0a73208c55ee0d27efe915399b1dc42a
khaled955 <khaledmansour104@yahoo.com>
2026-08-18 06:57:48 +0300

    finalize the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,59 @@
+import { PizzaData } from "./data/data";
+import { PizzaType } from "./types/pizza.type";
+
 export default function App() {
-  return <>
-  <h1> hello pizza</h1>
-  <img src="images/focaccia.jpg" alt="Pizza" />
-  </>;
+  return (
+    <main className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </main>
+  );
+}
+
+function Pizza({ pizza }: { pizza: PizzaType }) {
+  // Variables
+  const { ingredients, name, photoName, price,soldOut } = pizza;
+  return (
+    <li className={`pizza ${soldOut ? "sold-out" : ""}`}>
+      <img src={photoName} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>Price: {price}</span>
+      </div>
+    </li>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+function Menu() {
+  return (
+    <section className="menu">
+      <h2>Our menu</h2>
+      <ul className="pizzas">
+        {PizzaData.map((pizza) => (
+          <Pizza key={pizza.name} pizza={pizza} />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer() {
+  // Variables
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer className="footer">We are {isOpen ? "open" : "closed"} now</footer>
+  );
 }
